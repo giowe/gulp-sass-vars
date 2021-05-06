be0358e0674b2517f21657b8810ceba95831c048
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2021-05-06 22:44:19 +0200

    eslint fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const supportsColor = require("color-support")
 const colorDetectionOptions = {
   // If on Windows, ignore the isTTY check
   // This is due to AppVeyor (and thus probably common Windows platforms?) failing the check
-  ignoreTTY: (process.platform === "win32"),
+  ignoreTTY: (process.platform === "win32")
 }
 
 const defaultOptions = {
@@ -48,9 +48,10 @@ module.exports = (variables, options) => {
       try {
         value = parse(variables[name])
       } catch (error) {
-        if (options.verbose)
+        if (options.verbose) {
           log(`${pkg.name}: skipping var ${green(name)}.\n`
             + `${pkg.name}: ${error.message}`)
+        }
       }
 
       return `$${name}: ${value}`
@@ -61,16 +62,16 @@ module.exports = (variables, options) => {
       + `\t${statements.join("\n\t")}`)
   }
 
-  const stream = new Stream.Transform({objectMode: true})
+  const stream = new Stream.Transform({ objectMode: true })
 
   stream._transform = function(file, enc, cb) {
-    if(file.isNull()) {
+    if (file.isNull()) {
       return cb(null, file)
     }
 
     const prependedBuffer = Buffer.from(statements.join("\n"))
-    if(file.isStream()) {
-      file.contents = new StreamQueue( getStreamFromBuffer(prependedBuffer), file.contents)
+    if (file.isStream()) {
+      file.contents = new StreamQueue(getStreamFromBuffer(prependedBuffer), file.contents)
       return cb(null, file)
     }
 
